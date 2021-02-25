ddad3f08e010f5a69a50f72bb727ed7d456fb81a
Caio Carvalho <caiocarvalho.py@gmail.com>
2021-02-25 10:43:18 -0400

    test consultar_livros

--- colecao/livros.py
+++ colecao/livros.py
@@ -1,6 +1,17 @@
 def consultar_livros(autor):
-    preparar_dados_para_requisicao(autor)
-    return ""
+    dados = preparar_dados_para_requisicao(autor)
+    url = obter_url("https://buscador", dados)
+    ret = executar_requisicao(url)
+    return ret
+
 
 def preparar_dados_para_requisicao(autor):
+    pass
+
+
+def obter_url(url, dados):
+    pass
+
+
+def executar_requisicao(url):
     pass
